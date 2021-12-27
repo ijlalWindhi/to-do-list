4a7f47838929b817e838a7f497e6ec4dd4e44244
Ijlal Windhi <ijlalwindhi15@gmail.com>
2021-12-27 22:01:11 +0700

    first

---
+++ src/components/Todo.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+function Todo({ text, todo, todos, setTodos }) {
+  const deleteHandler = () => {
+    setTodos(todos.filter((el) => el.id !== todo.id));
+  };
+  return (
+    <div className="todo">
+      <li className="todo-item">{text}</li>
+      <button className="complete-btn">
+        <i className="fas fa-check"></i>
+      </button>
+      <button onClick={deleteHandler} className="trash-btn">
+        <i className="fas fa-trash"></i>
+      </button>
+    </div>
+  );
+}
+
+export default Todo;
---
+++ src/components/TodoList.js
@@ -0,0 +1,22 @@
+import React from "react";
+import Todo from "../components/Todo";
+
+function TodoList({ todos, setTodos }) {
+  return (
+    <div className="todo-container">
+      <ul className="todo-list">
+        {todos.map((todo) => (
+          <Todo
+            setTodos={setTodos}
+            todos={todos}
+            key={todo.id}
+            todo={todo}
+            text={todo.text}
+          />
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default TodoList;
